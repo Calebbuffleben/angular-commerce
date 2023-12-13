6dd85388e2d7dc0d885a7da5851bbea6785a3cee
Calebbuffleben <calebbuffleben@gmail.com>
2023-12-13 19:54:40 -0300

    product module

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ProductService } from './product.service';
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    AddProductComponent,
+    UpdateProductComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    ProductListComponent,
+    AddProductComponent,
+    UpdateProductComponent,
+  ],
+  providers: [ProductService]
 })
 export class ProductModule { }
